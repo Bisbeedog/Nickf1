8b69fce4ff3a40c9f6e226578c95ae31e2aaa0ee
Bisbeedog <nicholasprescottbisbee@gmail.com>
2021-06-20 10:14:43 -0700

    Update data.js

--- data.js
+++ data.js
@@ -184,14 +184,14 @@ var data = {
         {
             Name: "French Grand Prix",
             Date: "2021-06-20",
-            // Finish: [44, 77, 3, 4, 5, 6, 7, 9, 10, 11, 14, 16, 18, 22, 31, 33, 47, 55, 63, 99],
-            // Picks: [
-            //     { Player: "Nick", Drivers: [4, 99, 11] },
-            //     { Player: "Bridget", Drivers: [18, 63, 44] },
-            //     { Player: "DOD", Drivers: [33, 14, 22] },
-            //     { Player: "Logan", Drivers: [77, 5, 31] },
-            //     { Player: "Pud", Drivers: [3, 16, 7] }
-            // ]
+            Finish: [33, 44, 11, 77, 4, 3, 10, 14, 5, 18, 55, 63, 22, 31, 99, 16, 7, 6, 47, 9],
+            Picks: [
+                { Player: "Nick", Drivers: [4, 99, 11] },
+                 { Player: "Bridget", Drivers: [18, 63, 44] },
+                 { Player: "DOD", Drivers: [33, 14, 22] },
+                 { Player: "Logan", Drivers: [77, 5, 31] },
+                 { Player: "Pud", Drivers: [3, 16, 7] }
+             ]
         },
         {
             Name: "Styrian Grand Prix",
