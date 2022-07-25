f6c35df7a45893934e371d2d3067d6c4967422dd
Bisbeedog <nicholasprescottbisbee@gmail.com>
2022-07-24 20:45:43 -0700

    Update data.js

--- data.js
+++ data.js
@@ -249,14 +249,14 @@ var data = {
         {
             Name: "Grand Prix de France",
             Date: "2022-08-24",
-            // Finish: [44, 33, 77, 16, 11, 3, 55, 4, 31, 10, 18, 7, 5, 63, 99, 6, 14, 47, 9, 22],
-            // Picks: [
-            //     { Player: "Nick", Drivers: [11, 4, 99] },
-            //     { Player: "Bridget", Drivers: [44, 18, 63] },
-            //     { Player: "DOD", Drivers: [33, 14, 22] },
-            //     { Player: "Logan", Drivers: [77, 31, 5] },
-            //     { Player: "Pud", Drivers: [16, 3, 7] }
-            // ]
+            Finish: [1, 44, 63, 11, 55, 14, 4, 31, 3, 18, 5, 10, 23, 77, 47, 24, 6, 20, 16, 22],
+            Picks: [
+                { Player: "Nick", Drivers: [11, 4, 24] },
+                { Player: "Bridget", Drivers: [63, 3, 77] },
+                { Player: "DOD", Drivers: [1, 14, 22] },
+                { Player: "Logan", Drivers: [44, 5, 10] },
+                { Player: "Pud", Drivers: [55, 18, 16] }
+            ]
         },
         {
             Name: "Magyar Nagydig",
