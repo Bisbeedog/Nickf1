9427935e5fcdbcf031e0425a29a26cae6eb32c0b
Bisbeedog <nicholasprescottbisbee@gmail.com>
2022-09-04 21:47:57 -0700

    Update data.js

--- data.js
+++ data.js
@@ -285,14 +285,14 @@ var data = {
         {
             Name: "Dutch Grand Prix",
             Date: "2022-09-04",
-            // Finish: [44, 33, 77, 16, 11, 3, 55, 4, 31, 10, 18, 7, 5, 63, 99, 6, 14, 47, 9, 22],
-            // Picks: [
-            //     { Player: "Nick", Drivers: [11, 4, 99] },
-            //     { Player: "Bridget", Drivers: [44, 18, 63] },
-            //     { Player: "DOD", Drivers: [33, 14, 22] },
-            //     { Player: "Logan", Drivers: [77, 31, 5] },
-            //     { Player: "Pud", Drivers: [16, 3, 7] }
-            // ]
+            Finish: [1, 63, 16, 44, 11, 14, 4, 55, 31, 18, 10, 23, 47, 5, 20, 24, 3, 6, 77, 22],
+            Picks: [
+                 { Player: "Nick", Drivers: [11, 4, 24] },
+                 { Player: "Bridget", Drivers: [63, 3, 77] },
+                 { Player: "DOD", Drivers: [1, 14, 22] },
+                 { Player: "Logan", Drivers: [44, 10, 5] },
+                 { Player: "Pud", Drivers: [16, 55, 18] }
+             ]
         },
         {
             Name: "Gran Premio D'Italia",
