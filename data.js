8d9d656d4be04af8cb9cdc7934f2f83b76ba30df
Bisbeedog <nicholasprescottbisbee@gmail.com>
2022-10-09 10:46:51 -0700

    Update data.js

--- data.js
+++ data.js
@@ -326,14 +326,14 @@ var data = {
         {
             Name: "Japanese Grand Prix",
             Date: "2022-10-09",
-            // Finish: [44, 33, 77, 16, 11, 3, 55, 4, 31, 10, 18, 7, 5, 63, 99, 6, 14, 47, 9, 22],
-            // Picks: [
-            //     { Player: "Nick", Drivers: [11, 4, 99] },
-            //     { Player: "Bridget", Drivers: [44, 18, 63] },
-            //     { Player: "DOD", Drivers: [33, 14, 22] },
-            //     { Player: "Logan", Drivers: [77, 31, 5] },
-            //     { Player: "Pud", Drivers: [16, 3, 7] }
-            // ]
+            Finish: [1, 11, 16, 31, 44, 5, 14, 63, 6, 4, 3, 18, 22, 20, 77, 24, 47, 10, 55, 23],
+            Picks: [
+                 { Player: "Nick", Drivers: [11, 4, 24] },
+                 { Player: "Bridget", Drivers: [63, 3, 18] },
+                 { Player: "DOD", Drivers: [1, 14, 22] },
+                 { Player: "Logan", Drivers: [44, 5, 10] },
+                 { Player: "Pud", Drivers: [16, 18, 55] }
+             ]
         },
         {
             Name: "United States Grand Prix",
