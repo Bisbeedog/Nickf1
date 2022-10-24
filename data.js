c86bce7b5a4d209843911ef1c9d0e459ccec03ce
Bisbeedog <nicholasprescottbisbee@gmail.com>
2022-10-23 19:36:42 -0700

    Update data.js

--- data.js
+++ data.js
@@ -338,14 +338,14 @@ var data = {
         {
             Name: "United States Grand Prix",
             Date: "2022-10-23",
-            // Finish: [44, 33, 77, 16, 11, 3, 55, 4, 31, 10, 18, 7, 5, 63, 99, 6, 14, 47, 9, 22],
-            // Picks: [
-            //     { Player: "Nick", Drivers: [11, 4, 99] },
-            //     { Player: "Bridget", Drivers: [44, 18, 63] },
-            //     { Player: "DOD", Drivers: [33, 14, 22] },
-            //     { Player: "Logan", Drivers: [77, 31, 5] },
-            //     { Player: "Pud", Drivers: [16, 3, 7] }
-            // ]
+            Finish: [1, 44, 16, 11, 63, 4, 5, 20, 22, 31, 23, 24, 10, 47, 14, 3, 6, 18, 77, 55],
+            Picks: [
+                 { Player: "Nick", Drivers: [11, 4, 24] },
+                 { Player: "Bridget", Drivers: [63, 3, 18] },
+                 { Player: "DOD", Drivers: [1, 22, 14] },
+                 { Player: "Logan", Drivers: [44, 5, 10] },
+                 { Player: "Pud", Drivers: [16, 18, 55] }
+             ]
         },
         {
             Name: "Gran Premio de la Ciudad de Mexico",
