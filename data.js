a05c1e84e49c10a89f9b3d427d6f43d9562e729d
Bisbeedog <nghaga@csu.fullerton.edu>
2022-11-20 10:29:16 -0800

    Update data.js

--- data.js
+++ data.js
@@ -374,14 +374,14 @@ var data = {
         {
             Name: "Abu Dhabi Grand Prix",
             Date: "2022-11-20",
-            // Finish: [44, 33, 77, 16, 11, 3, 55, 4, 31, 10, 18, 7, 5, 63, 99, 6, 14, 47, 9, 22],
-            // Picks: [
-            //     { Player: "Nick", Drivers: [11, 4, 99] },
-            //     { Player: "Bridget", Drivers: [44, 18, 63] },
-            //     { Player: "DOD", Drivers: [33, 14, 22] },
-            //     { Player: "Logan", Drivers: [77, 31, 5] },
-            //     { Player: "Pud", Drivers: [16, 3, 7] }
-            // ]
+            Finish: [25, 18, 11, 55, 63, 4, 31, 18, 3, 5, 22, 24, 23, 10, 77, 47, 20, 44, 6, 14],
+            Picks: [
+                 { Player: "Nick", Drivers: [11, 4, 99] },
+                 { Player: "Bridget", Drivers: [44, 18, 63] },
+                 { Player: "DOD", Drivers: [33, 14, 22] },
+                 { Player: "Logan", Drivers: [77, 31, 5] },
+                 { Player: "Pud", Drivers: [16, 3, 7] }
+             ]
         }
     ]
 }
